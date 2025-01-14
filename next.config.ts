49fd989c72321140d7f478171118464a1f5a49ff
Meyyappan <meyyappan721@gmail.com>
2025-01-14 21:46:12 +0530

    hero page

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,20 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'placehold.co',
+        // pathname: "/path/to/images/**"
+      },
+      {
+        protocol: 'https',
+        hostname: 'm.media-amazon.com',
+        // pathname: "/path/to/images/**"
+      },
+    ],
+  },
 };
 
 export default nextConfig;
